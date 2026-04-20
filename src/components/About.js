e0de101a4fb718d9c8a369b97226deb479178649
arshiya-hamdani <arshiyahamdani@gmail.com>
2026-04-20 18:37:10 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,8 @@ const About = () => {
       <h2>let me introduce myself</h2>
       <div className="about-content">
        <p>
-          Hello, I'm <span className="highlight">Arshiya</span>. I hold a **Master’s degree from Parul University**, building upon my foundation as a Bachelor of Computer Applications (BCA) graduate from BKMNU University (2021-2024). 
-        </p>
+  Hello, I'm <span className="highlight">Arshiya</span>. I have successfully completed my **Master of Computer Applications (MCA) from Parul University**, following my Bachelor of Computer Applications (BCA) from BKMNU University (2021-2024).
+</p>
         <p>
           During my academic journey, I completed various projects that strengthened my technical expertise and problem-solving abilities. My experience with technologies like React.js, Bootstrap, and MySQL has allowed me to build dynamic and responsive web applications.
         </p>
